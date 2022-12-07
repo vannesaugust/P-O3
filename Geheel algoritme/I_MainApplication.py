17668c4135f4d74b530ef608e8b49018425c682b
RubenVanherpe <ruben.vanherpe@outlook.com>
2022-12-07 11:46:42 +0100

    aanpassingen

--- Geheel algoritme/I_MainApplication.py
+++ Geheel algoritme/I_MainApplication.py
@@ -331,7 +331,7 @@ gegevens_uit_database_halen()
 lijst_apparaten = ['warmtepomp', 'droogkast', 'wasmachine', 'frigo', 'vaatwas', 'diepvries', 'elektrische auto']
 lijst_soort_apparaat = ['/', 'Consumer', 'Consumer', 'Always on', 'Consumer', 'Always on', 'Device with battery']
 lijst_capaciteit = ['/', '/', '/', '/', '/', '/', 20]
-lijst_aantal_uren = ['/', 2, 1, 2, 2, 2, 3]
+lijst_aantal_uren = ['/', 2, 1,10, 2, 10, 3]
 lijst_uren_na_elkaar = ['/', 2, '/', '/', '/', '/', '/']
 lijst_verbruiken = [2, 0.5, 3, 1.2, 0.8, 2.1, 7]
 lijst_deadlines = ['/', 10, 11, '/', 30, '/', 5]
@@ -2118,14 +2118,10 @@ class HomeFrame(CTkFrame):
 
             # frame batterijen updaten
             kWh_bij_of_af = batterij_niveau - oud_batterijniveau
-            print('HIER MOET JE ZIJN VOOR DE BATTERIJEN:')
-            print(oud_batterijniveau)
-            print(batterij_niveau)
-            print(totale_batterijcapaciteit)
             if totale_batterijcapaciteit == 0:
                 percentage = 100
             else:
-                percentage = int((batterij_niveau / totale_batterijcapaciteit) * 100)
+                percentage = int((oud_batterijniveau / totale_batterijcapaciteit) * 100)
             print(percentage)
             label_percentage.configure(text=str(percentage) + '%')
             if kWh_bij_of_af > 0:
@@ -2406,8 +2402,8 @@ class FrameTemperatuur(CTkFrame):
                 cur.execute("UPDATE Huisgegevens SET UWaarde =" + str(U_waarde))
                 cur.execute("UPDATE Huisgegevens SET OppervlakteMuren =" + str(oppervlakte_muren))
                 cur.execute("UPDATE Huisgegevens SET VolumeHuis =" + str(volume_huis))
-                cur.execute("UPDATE Geheugen SET Wattages =" + str(verbruik_warmtepomp) +
-                            " WHERE Nummering =" + 0)
+                cur.execute("UPDATE OudGeheugen SET Wattages =" + str(verbruik_warmtepomp) +
+                            " WHERE Nummering =" + str(0)) #GOED DAT DIT IN OUDGEHEUGEN WORDT GEUPDATE??????
                 con.commit()
                 cur.close()
                 con.close()
