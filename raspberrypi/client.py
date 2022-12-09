c286c49269b9dfb3337c7debf9e0022d5363c001
esat3b1 <vannes.august@gmail.com>
2022-12-09 15:20:30 +0100

    raspberry pi programs

---
+++ raspberrypi/client.py
@@ -0,0 +1,21 @@
+# echo-client.py
+
+import socket
+import pickle
+
+HOST = "MacBook-Pro-van-August.local"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+print (HOST)
+print (PORT)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+
+while True:
+    msg = s.recv(1024)
+    #data = msg.decode("utf-8")
+    leds = pickle.loads(msg)
+    print(leds)
+
+
+
