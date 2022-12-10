c04701870b55a0c483534657b4db9d72cf5edfa6
landerverhoeven <lander.verhoeven@student.kuleuven.be>
2022-12-10 12:56:19 +0100

    prijzen per maand

--- Geheel algoritme/I_MainApplication.py
+++ Geheel algoritme/I_MainApplication.py
@@ -2599,6 +2599,8 @@ class HomeFrame(CTkFrame):
             global kost_met_optimalisatie, kost_zonder_optimalisatie
             kost_met_optimalisatie += (from_grid + grid_to_battery - to_grid - battery_to_grid)*Prijzen24uur[0]
             PrijzenMaandelijks = [0, 0.3, 0.3, 0.29, 0.35, 0.33, 0.31, 0.33, 0.47, 0.77, 0.17, 0.21, 0.19]
+            # De prijzen hieronder zijn gemiddelde berekend uit de Belpex
+            PrijzenMaandelijks = [0.16524, 0.20215, 0.24544, 0.19140, 0.16264, 0.26571, 0.18659, 0.17664, 0.21910, 0.32133, 0.44813, 0.34886]
             print('kost met optimalisatie interface: ',kost_met_optimalisatie)
             maand = int(current_date[3:5])
             print("2kost_zonder_optimalisatie------------------------------------------------------------------")
--- Geheel algoritme/testen koppeling database interface.py
+++ Geheel algoritme/testen koppeling database interface.py
@@ -2,5 +2,23 @@ import sqlite3
 
 con = sqlite3.connect("D_VolledigeDatabase.db")
 cur = con.cursor()
-res = cur.execute("SELECT * FROM Weer")
-print(res.fetchall())
+res = cur.execute("SELECT * FROM Stroomprijzen")
+# print(res.fetchall())
+res = cur.execute("SELECT Prijs FROM Stroomprijzen WHERE DatumBelpex LIKE '%/09/2022%'")
+print("voor een maand:")
+waarden_maand = res.fetchall()
+print(waarden_maand)
+totale_prijs = 0
+
+for i in waarden_maand:
+    prijs = i[0]
+    print(prijs)
+    prijs = prijs[4:]
+    prijs = prijs[0:-3] + '.' + prijs[-2:]
+    print(prijs)
+    prijs = float(prijs)
+    totale_prijs += prijs
+gem_prijs = totale_prijs / len(waarden_maand)
+print('gem_prijs is:')
+print(gem_prijs)
+print(len(waarden_maand))
