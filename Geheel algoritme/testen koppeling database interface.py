4630a6a67e61af12899c60df48ee2137d559fb82
landerverhoeven <lander.verhoeven@student.kuleuven.be>
2022-12-13 12:07:27 +0100

    update

--- Geheel algoritme/testen koppeling database interface.py
+++ Geheel algoritme/testen koppeling database interface.py
@@ -3,7 +3,8 @@ import sqlite3
 con = sqlite3.connect("D_VolledigeDatabase.db")
 cur = con.cursor()
 res = cur.execute("SELECT * FROM Stroomprijzen")
-# print(res.fetchall())
+print(res.fetchall())
+'''
 res = cur.execute("SELECT Prijs FROM Stroomprijzen WHERE DatumBelpex LIKE '%/09/2022%'")
 print("voor een maand:")
 waarden_maand = res.fetchall()
@@ -22,3 +23,4 @@ gem_prijs = totale_prijs / len(waarden_maand)
 print('gem_prijs is:')
 print(gem_prijs)
 print(len(waarden_maand))
+'''
